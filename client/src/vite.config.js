df4a134076b45c80bdc828ace775438e8b355f5f
Yashica Lohar <loharyashica@outlook.com>
2026-07-26 12:52:21 +0530

    fix: client issues

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss(),
+  // tailwindcss(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Raises the warning limit to 1000 kB (1MB)
+  },
 })
