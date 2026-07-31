8d72391ed248024895a5c07dd76078afca6dacc9
Yashica Lohar <loharyashica@outlook.com>
2026-07-31 11:38:12 +0530

    fix analytics dashboard errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
-import { BrowserRouter, Routes, Route, Router} from "react-router";
-import Navbar from './components/Navbar'
-import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import Mail from "./components/Mail";
 import Helpdesk from "./components/Helpdesk";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import AnalyticsDashboard from "./components/Analyticsdashboard";
+import AnalyticsDashboard from "./components/dashboard";
 
 function App() {
+  const fallbackAccountId = "96b0d249-61d6-11f1-adde-e86538d58b3c";
 
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/mail' element={<Mail />} />
-            <Route path='/analyticsDashboard' element={<AnalyticsDashboard />} />
-            <Route path='/helpdesk' element={<Helpdesk />} />
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/signup' element={<Signup/>}/>
-          </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/mail' element={<Mail />} />
+        <Route path='/analyticsDashboard' element={<AnalyticsDashboard accountId={fallbackAccountId} />} />
+        <Route path='/helpdesk' element={<Helpdesk />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
