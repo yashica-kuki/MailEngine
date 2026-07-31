66123bd587aaea45a5355a33cbebcfcbddda0659
Yashica Lohar <loharyashica@outlook.com>
2026-07-31 11:49:18 +0530

    fix errors

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,4 +1,4 @@
-import AnalyticsDashboard from './AnalyticsDashboard';
+import AnalyticsDashboard from './Analyticsdashboard';
 
 function App() {
   const currentAccountId = "96b0d249-61d6-11f1-adde-e86538d58b3c";
